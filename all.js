cb48e7f066fab687f375ead7616f875486be40cb
susu3131 <anny81327@gmail.com>
2022-11-17 23:07:56 +0800

    第六周作業

--- all.js
+++ all.js
@@ -1,6 +1,6 @@
 //共寫三個功能 ，一、代入套票資料  2. 篩選 3. 新增套票
 
-import axios, {isCancel, AxiosError, Axios} from 'axios';
+import axios from './node_modules/axios/dist/esm/axios.js';
 
 //共寫三個功能 ，一、代入套票資料  2. 篩選 3. 新增套票
 let data = [];
@@ -10,9 +10,11 @@ axios.get('https://raw.githubusercontent.com/hexschool/js-training/main/travelAp
     console.log(response.data.data);
     
   })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
 
-
-console.log (data)
 // 一、代入套票資料
 // let data = [
 //   {
@@ -119,133 +121,133 @@ const selectTaichung = document.querySelector('.taichung')
 const selectKaohsiung = document.querySelector('.kaohsiung')
 
 //2. 監聽地區搜尋內容 選取物件，並測試是否成功取值
-// selectCounty.addEventListener("click",function(e){
-  
-// //3. 寫判斷式是否點選值與data.area相同
-//   if(e.target.value == selectAll.textContent){ //第1為全部，判斷e.target.value是否為"全部地區"
-//     dataAll() //全部筆數 
-//     addHTML() //新增
-//     searchNum() //搜尋筆數
-//     content="" // 重置避免點擊BUG
+selectCounty.addEventListener("click",function(e){
     
-//   }
-//   else if(e.target.value == selectTaipei.textContent){ //第2為全部，判斷e.target.value是否為"台北"
-//     data.forEach(function(item,num){
-//       if(e.target.value == item.area){
-//         search()
-//       content += `<li class="ticketCard">
-//       <div class="ticketCard-img">
-//         <a href="#">
-//           <img src="${item.imgUrl}" alt="">
-//         </a>
-//         <div class="ticketCard-region">${item.area}</div>
-//         <div class="ticketCard-rank">${item.rate}</div>
-//       </div>
-//       <div class="ticketCard-content">
-//         <div>
-//           <h3>
-//             <a href="#" class="ticketCard-name">${item.name}</a>
-//           </h3>
-//           <p class="ticketCard-description">
-//             ${item.description}
-//           </p>
-//         </div>
-//         <div class="ticketCard-info">
-//           <p class="ticketCard-num">
-//             <span><i class="fas fa-exclamation-circle"></i></span>
-//             剩下最後 <span id="ticketCard-num"> ${item.group} </span> 組
-//           </p>
-//           <p class="ticketCard-price">
-//             TWD <span id="ticketCard-price">$${item.price}</span>
-//           </p>
-//         </div>
-//       </div>
-//     </li>`  
-//     }
-//     });
-//     addHTML()
-//     searchNum()
-//     content=""
-//   }
-//   else if(e.target.value == selectTaichung.textContent){  //第3為全部，判斷e.target.value是否為"台中"
-//     data.forEach(function(item,num){
-//       if(e.target.value == item.area){
-//       search()
-//       content += `<li class="ticketCard">
-//       <div class="ticketCard-img">
-//         <a href="#">
-//           <img src="${item.imgUrl}" alt="">
-//         </a>
-//         <div class="ticketCard-region">${item.area}</div>
-//         <div class="ticketCard-rank">${item.rate}</div>
-//       </div>
-//       <div class="ticketCard-content">
-//         <div>
-//           <h3>
-//             <a href="#" class="ticketCard-name">${item.name}</a>
-//           </h3>
-//           <p class="ticketCard-description">
-//             ${item.description}
-//           </p>
-//         </div>
-//         <div class="ticketCard-info">
-//           <p class="ticketCard-num">
-//             <span><i class="fas fa-exclamation-circle"></i></span>
-//             剩下最後 <span id="ticketCard-num"> ${item.group} </span> 組
-//           </p>
-//           <p class="ticketCard-price">
-//             TWD <span id="ticketCard-price">$${item.price}</span>
-//           </p>
-//         </div>
-//       </div>
-//     </li>`  
-//     }
-//     });
-//     addHTML()
-//     searchNum()
-//     content=""
+//3. 寫判斷式是否點選值與data.area相同
+  if(e.target.value == selectAll.textContent){ //第1為全部，判斷e.target.value是否為"全部地區"
+    dataAll() //全部筆數 
+    addHTML() //新增
+    searchNum() //搜尋筆數
+    content="" // 重置避免點擊BUG
+    
+  }
+  else if(e.target.value == selectTaipei.textContent){ //第2為全部，判斷e.target.value是否為"台北"
+    data.forEach(function(item,num){
+      if(e.target.value == item.area){
+        search()
+      content += `<li class="ticketCard">
+      <div class="ticketCard-img">
+        <a href="#">
+          <img src="${item.imgUrl}" alt="">
+        </a>
+        <div class="ticketCard-region">${item.area}</div>
+        <div class="ticketCard-rank">${item.rate}</div>
+      </div>
+      <div class="ticketCard-content">
+        <div>
+          <h3>
+            <a href="#" class="ticketCard-name">${item.name}</a>
+          </h3>
+          <p class="ticketCard-description">
+            ${item.description}
+          </p>
+        </div>
+        <div class="ticketCard-info">
+          <p class="ticketCard-num">
+            <span><i class="fas fa-exclamation-circle"></i></span>
+            剩下最後 <span id="ticketCard-num"> ${item.group} </span> 組
+          </p>
+          <p class="ticketCard-price">
+            TWD <span id="ticketCard-price">$${item.price}</span>
+          </p>
+        </div>
+      </div>
+    </li>`  
+    }
+    });
+    addHTML()
+    searchNum()
+    content=""
+  }
+  else if(e.target.value == selectTaichung.textContent){  //第3為全部，判斷e.target.value是否為"台中"
+    data.forEach(function(item,num){
+      if(e.target.value == item.area){
+      search()
+      content += `<li class="ticketCard">
+      <div class="ticketCard-img">
+        <a href="#">
+          <img src="${item.imgUrl}" alt="">
+        </a>
+        <div class="ticketCard-region">${item.area}</div>
+        <div class="ticketCard-rank">${item.rate}</div>
+      </div>
+      <div class="ticketCard-content">
+        <div>
+          <h3>
+            <a href="#" class="ticketCard-name">${item.name}</a>
+          </h3>
+          <p class="ticketCard-description">
+            ${item.description}
+          </p>
+        </div>
+        <div class="ticketCard-info">
+          <p class="ticketCard-num">
+            <span><i class="fas fa-exclamation-circle"></i></span>
+            剩下最後 <span id="ticketCard-num"> ${item.group} </span> 組
+          </p>
+          <p class="ticketCard-price">
+            TWD <span id="ticketCard-price">$${item.price}</span>
+          </p>
+        </div>
+      </div>
+    </li>`  
+    }
+    });
+    addHTML()
+    searchNum()
+    content=""
 
-//   }
-//   else if(e.target.value == selectKaohsiung.textContent){ //第4為全部，判斷e.target.value是否為"高雄"
-//     data.forEach(function(item,num){
-//       if(e.target.value == item.area){
-//       search()
-//       content += `<li class="ticketCard">
-//       <div class="ticketCard-img">
-//         <a href="#">
-//           <img src="${item.imgUrl}" alt="">
-//         </a>
-//         <div class="ticketCard-region">${item.area}</div>
-//         <div class="ticketCard-rank">${item.rate}</div>
-//       </div>
-//       <div class="ticketCard-content">
-//         <div>
-//           <h3>
-//             <a href="#" class="ticketCard-name">${item.name}</a>
-//           </h3>
-//           <p class="ticketCard-description">
-//             ${item.description}
-//           </p>
-//         </div>
-//         <div class="ticketCard-info">
-//           <p class="ticketCard-num">
-//             <span><i class="fas fa-exclamation-circle"></i></span>
-//             剩下最後 <span id="ticketCard-num"> ${item.group} </span> 組
-//           </p>
-//           <p class="ticketCard-price">
-//             TWD <span id="ticketCard-price">$${item.price}</span>
-//           </p>
-//         </div>
-//       </div>
-//     </li>`  
-//     }
-//     });
-//     addHTML()
-//     searchNum()
-//     content=""
-//   }
+  }
+  else if(e.target.value == selectKaohsiung.textContent){ //第4為全部，判斷e.target.value是否為"高雄"
+    data.forEach(function(item,num){
+      if(e.target.value == item.area){
+      search()
+      content += `<li class="ticketCard">
+      <div class="ticketCard-img">
+        <a href="#">
+          <img src="${item.imgUrl}" alt="">
+        </a>
+        <div class="ticketCard-region">${item.area}</div>
+        <div class="ticketCard-rank">${item.rate}</div>
+      </div>
+      <div class="ticketCard-content">
+        <div>
+          <h3>
+            <a href="#" class="ticketCard-name">${item.name}</a>
+          </h3>
+          <p class="ticketCard-description">
+            ${item.description}
+          </p>
+        </div>
+        <div class="ticketCard-info">
+          <p class="ticketCard-num">
+            <span><i class="fas fa-exclamation-circle"></i></span>
+            剩下最後 <span id="ticketCard-num"> ${item.group} </span> 組
+          </p>
+          <p class="ticketCard-price">
+            TWD <span id="ticketCard-price">$${item.price}</span>
+          </p>
+        </div>
+      </div>
+    </li>`  
+    }
+    });
+    addHTML()
+    searchNum()
+    content=""
+  }
 
-// })
+})
   
 
 // 三、 新增套票
@@ -277,10 +279,10 @@ btn.addEventListener("click",function(e){
     "rate": ticketRate.value,
   }
   //push上去data陣列資料裡
+  
   data.push(dataPost)
   dataAll()
   addHTML()
   searchNum() 
 })
-
 
