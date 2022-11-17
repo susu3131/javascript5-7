265d00ee9155fbcaa5e27ce9d6850a2d20e91e02
susu3131 <anny81327@gmail.com>
2022-11-17 23:15:27 +0800

    axios

--- all.js
+++ all.js
@@ -1,6 +1,6 @@
 //共寫三個功能 ，一、代入套票資料  2. 篩選 3. 新增套票
 
-import axios from './node_modules/axios/dist/esm/axios.js';
+import axios from './javascript5-7/node_modules/axios/dist/esm/axios.js';
 
 //共寫三個功能 ，一、代入套票資料  2. 篩選 3. 新增套票
 let data = [];
